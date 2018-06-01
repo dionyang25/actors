9e102257180e94fe23cc8e4d30f82b43691c3b32
yanghaonan <yanghaonan@sh.hupu.com>
2018-06-01 18:08:54 +0800

    增加资源显示

--- src/config/users.php
+++ src/config/users.php
@@ -7,6 +7,18 @@
  */
 $config['users']['game_initial'] = [
     'hp'=>15,
-    'card_num'=>0
+    'card_num'=>0,
+    'resource'=>[
+        1=>0,2=>0,3=>0
+    ]
+];
+
+$config['users']['resource'] =[
+    1 =>'fire',
+    2 =>'water',
+    3 =>'wind',
+    'fire'=>1,
+    'water'=>2,
+    'wind'=>3
 ];
 return $config;
