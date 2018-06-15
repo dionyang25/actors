f543c4b1271ae2559d4e8ce25faec699845e0acf
yanghaonan <yanghaonan@sh.hupu.com>
2018-06-15 18:40:49 +0800

    版本中去除不必要的文件-2

--- src/app/Controllers/UserController.php
+++ src/app/Controllers/UserController.php
@@ -9,12 +9,22 @@ namespace app\Controllers;
 use Server\CoreBase\Controller;
 
 class UserController extends BaseController {
+    private $table = 'card_user';
     /**
      * 注册
      */
     public function http_register(){
+
         $username = $this->http_input->post('username');
         $password = $this->http_input->post('password');
+        if(empty($username) || empty($password)){
+           return ;
+        }
+        $password = md5($password);
+        try{
+            $this->mysql_pool->dbQueryBuilder->insert($this->table);
+        }catch (\Exception $e){
 
+        }
     }
 }
